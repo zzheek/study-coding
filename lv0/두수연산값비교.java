2a7d84e4b924fb3e282d3e6bc94f63b63b998c1b
강주희 <133830185+zzheek@users.noreply.github.com>
2023-11-23 21:22:03 +0900

    Create 두수연산값비교.java

---
+++ lv0/두수연산값비교.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        answer=(Integer.parseInt(a+""+b+"")>(2*a*b))?
+            Integer.parseInt(a+""+b+""):(2*a*b);
+
+        
+        return answer;
+    }
+}
