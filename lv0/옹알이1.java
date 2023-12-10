ae8f97a79d012a834ab6f57f1298574f78941f71
강주희 <133830185+zzheek@users.noreply.github.com>
2023-12-10 18:11:53 +0900

    Create 옹알이1.java

---
+++ lv0/옹알이1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(int i=0; i<babbling.length; i++){
+            if(babbling[i].matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$")){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
+
+
+
+
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+
+        for(int i =0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replace("aya", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
+            if(babbling[i].isEmpty()) {
+                answer = answer + 1;
+            }
+        }
+
+        return answer;
+    }
+}
