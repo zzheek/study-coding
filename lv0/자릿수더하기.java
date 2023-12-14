f3d6fe2c8c2b17a96a2da837112956b1e8bf96d4
강주희 <133830185+zzheek@users.noreply.github.com>
2023-12-14 13:01:16 +0900

    Create 자릿수더하기.java

---
+++ lv0/자릿수더하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            answer += n % 10; 
+            n /= 10; 
+        }
+        return answer;
+    }
+}
+
+
+class Solution {
+    public int solution(int n) {
+        return String.valueOf(n).chars().map(Character::getNumericValue).sum();
+    }
+}
