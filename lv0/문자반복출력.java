d0223d590efa12115d828c702871095d848628c0
강주희 <133830185+zzheek@users.noreply.github.com>
2023-12-19 16:44:46 +0900

    Create 문자반복출력.java

---
+++ lv0/문자반복출력.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        
+        for(int i=0; i<my_string.length(); i++) {
+            for(int j=0; j<n; j++) {
+            answer += my_string.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
+
+
+
+
+class Solution {
+    public String solution(String my_string, int n) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : my_string.toCharArray()){
+            sb.append((c + "").repeat(n));
+        }
+        return sb.toString();
+    }
+}
