8006b07d265e288b0d9c7e360940e33ddd93fc3c
강주희 <133830185+zzheek@users.noreply.github.com>
2023-12-20 15:53:58 +0900

    Create 배열자르기.java

---
+++ lv0/배열자르기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2 - num1 + 1];
+        for (int i=num1; i<=num2; i++) {
+            answer[i - num1] = numbers[i];
+        }
+        
+        return answer;
+    }
+}
+
+
+
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        return Arrays.copyOfRange(numbers, num1, num2 + 1);
+    }
+}
