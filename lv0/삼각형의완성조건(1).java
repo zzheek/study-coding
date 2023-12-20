64168402f40f2ae3c4a3cc74396efb891cb23172
강주희 <133830185+zzheek@users.noreply.github.com>
2023-12-20 15:10:42 +0900

    Create 삼각형의완성조건(1).java

---
+++ lv0/삼각형의완성조건(1).java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] sides) {
+        Arrays.sort(sides);
+        int answer = (sides[2]<(sides[0]+sides[1]))? 1:2 ;
+
+        return answer;
+    }
+}
+
+
