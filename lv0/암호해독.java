f9b56fd7a5da98a2ec83a0112dcee4b2b4711aac
강주희 <133830185+zzheek@users.noreply.github.com>
2023-12-22 14:04:01 +0900

    Create 암호해독.java

---
+++ lv0/암호해독.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+        
+        for(int i=code-1; i<cipher.length(); i+=code) {
+            answer += cipher.charAt(i);
+        }
+        
+        return answer;
+    }
+}
+
+
+
+
+class Solution {
+    public String solution(String cipher, int code) {
+        String answer = "";
+
+        for (int i = code; i <= cipher.length(); i = i + code) {
+            answer += cipher.substring(i - 1, i);
+        }
+
+        return answer;
+    }
+}
