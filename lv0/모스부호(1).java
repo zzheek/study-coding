537838c81cf9f74ca974a25fff487b1aa384c993
강주희 <133830185+zzheek@users.noreply.github.com>
2023-12-26 12:47:32 +0900

    Create 모스부호(1).java

---
+++ lv0/모스부호(1).java
@@ -0,0 +1,34 @@
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        
+        return answer = letter .replace(".-..","l")
+                        .replace(".--.","p")
+                        .replace("-...","b")
+                        .replace("-.-.","c")
+                        .replace("..-.","f")
+                        .replace("....","h")
+                        .replace(".---","j")
+                        .replace("--.-","q")
+                        .replace("-..-","x")
+                        .replace("-.--","y")
+                        .replace("--..","z")
+                        .replace("...-","v")
+                        .replace("---","o")
+                        .replace("-..","d")
+                        .replace("-.-","k")
+                        .replace("--.","g")
+                        .replace(".-.","r")
+                        .replace("...","s")
+                        .replace(".--","w")
+                        .replace("..-","u")
+                        .replace(".-","a")
+                        .replace("..","i")
+                        .replace("--","m")
+                        .replace("-.","n")
+                        .replace("-","t")
+                        .replace(".","e")
+                        .replace(" ","");
+        
+    }
+}
