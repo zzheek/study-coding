478ebf6aa2d5c4973f92d1f41212daca6f10c620
강주희 <133830185+zzheek@users.noreply.github.com>
2024-01-01 17:47:40 +0900

    Create 뒤에서5등까지.java

---
+++ lv0/뒤에서5등까지.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[5];
+        Arrays.sort(num_list);
+        for(int i=0; i<5; i++) {
+            answer[i] = num_list[i];
+        }
+        return answer;
+    }
+}
+
+
+
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+
+        return Arrays.copyOfRange(num_list, 0, 5);
+    }
+}
