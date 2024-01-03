167b828f6a0fde22784439eeb30f5ba819c225fc
강주희 <133830185+zzheek@users.noreply.github.com>
2024-01-03 14:19:30 +0900

    Create 배열의원소만큼추가하기.java

---
+++ lv0/배열의원소만큼추가하기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> result = new ArrayList<>();
+
+        for (int num : arr) {
+            for (int i = 0; i < num; i++) {
+                result.add(num);
+            }
+        }
+
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+        
+        return answer;
+        
+    }
+}
+
+
+
+
