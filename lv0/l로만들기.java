3c4bfb7c671e235c196919f683296b0ee9e44aad
강주희 <133830185+zzheek@users.noreply.github.com>
2024-01-04 13:44:19 +0900

    Create l로만들기.java

---
+++ lv0/l로만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String myString) {
+        return myString.replaceAll("[a-k]","l");
+    }
+}
+
+
+
+
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+
+        for (int i = 0; i < myString.length(); i++) {
+            if (myString.charAt(i) <= 'l') {
+                answer += "l";
+            } else {
+                answer += myString.charAt(i) + "";
+            }
+        }
+        return answer;
+    }
+}
