eb5b945fb730042a685929550953f62d37e0dec1
강주희 <133830185+zzheek@users.noreply.github.com>
2024-01-04 11:43:34 +0900

    Create 0떼기.java

---
+++ lv0/0떼기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(String n_str) {
+        
+         while (n_str.startsWith("0")) {
+            n_str = n_str.substring(1);
+        }
+        
+        return n_str;
+    }
+}
+
+
+
+
+class Solution {
+    public String solution(String n_str) {
+        return ""+Integer.parseInt(n_str);
+    }
+}
