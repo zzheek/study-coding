96c295b0dd1c4fd37a117b46c880f579e7e056b6
강주희 <133830185+zzheek@users.noreply.github.com>
2024-01-05 10:52:16 +0900

    Create 특별한이차원배열1.java

---
+++ lv0/특별한이차원배열1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(i==j) {
+                    answer[i][j] = 1;
+                } else {
+                    answer[i][j] = 0;
+                }
+            }
+        }
+        return answer;
+    }
+}
+
+
+
+
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = new int[n][n];
+        for(int i = 0 ; i < n ; i++) {
+            answer[i][i] = 1;
+        }
+        return answer;
+    }
+}
