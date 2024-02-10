cf812f912ea0de8ef5779ffc23da64fec7b9eb51
강주희 <133830185+zzheek@users.noreply.github.com>
2024-02-10 11:10:54 +0900

    Create 문자열 내림차순으로 배치하기.java

---
+++ lv1/문자열 내림차순으로 배치하기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        char[] ch = s.toCharArray();
+        
+        Arrays.sort(ch);
+        
+        for(int i=ch.length-1; i>=0; i--) {
+            answer += ch[i];
+        }
+        
+        return answer;
+    }
+}
+
+
+
