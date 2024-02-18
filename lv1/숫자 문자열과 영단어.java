129961fead0cd2805de18c1be43da19bc269b16c
강주희 <133830185+zzheek@users.noreply.github.com>
2024-02-18 12:35:14 +0900

    Create 숫자 문자열과 영단어.java

---
+++ lv1/숫자 문자열과 영단어.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+
+class Solution {
+    public int solution(String s) {
+        HashMap<String, String> numbers = new HashMap<>();
+        numbers.put("zero", "0");
+        numbers.put("one", "1");
+        numbers.put("two", "2");
+        numbers.put("three", "3");
+        numbers.put("four", "4");
+        numbers.put("five", "5");
+        numbers.put("six", "6");
+        numbers.put("seven", "7");
+        numbers.put("eight", "8");
+        numbers.put("nine", "9");
+
+        for (String key : numbers.keySet()) {
+            s = s.replace(key, numbers.get(key));
+        }
+
+        return Integer.parseInt(s);
+    }
+}
+
+
+
+
+class Solution {
+    public int solution(String s) {
+        String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        for(int i = 0; i < strArr.length; i++) {
+            s = s.replaceAll(strArr[i], Integer.toString(i));
+        }
+        return Integer.parseInt(s);
+    }
+}
