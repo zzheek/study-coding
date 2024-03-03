5192e892c12d6ff8615cc007c660139e66a0defa
강주희 <133830185+zzheek@users.noreply.github.com>
2024-03-03 11:31:50 +0900

    Create 소수 만들기.java

---
+++ lv1/소수 만들기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        // 3개의 수를 뽑아서 그 합이 소수인 경우 찾기
+        for (int i=0; i<nums.length-2; i++) {
+            for (int j=i+1; j<nums.length-1; j++) {
+                for (int k=j+1; k <nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+    
+    private boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
